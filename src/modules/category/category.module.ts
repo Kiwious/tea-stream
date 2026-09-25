7a5e0d496ea9c1c08fa4606480388d0623b8bea2
Kiwious <novikov.kevin1@gmail.com>
2026-09-25 18:58:15 +0200

    feat(category): add category resolver, include streams in query

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 
+import { CategoryResolver } from './category.resolver'
 import { CategoryService } from './category.service'
 
 @Module({
-	providers: [CategoryService]
+	providers: [CategoryService, CategoryResolver]
 })
 export class CategoryModule {}
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -10,6 +10,14 @@ export class CategoryService {
 		const categories = await this.prismaService.category.findMany({
 			orderBy: {
 				createdAt: 'desc'
+			},
+			include: {
+				streams: {
+					include: {
+						user: true,
+						category: true
+					}
+				}
 			}
 		})
 		return categories
@@ -28,7 +36,15 @@ export class CategoryService {
 
 		const categories = await this.prismaService.category.findMany({
 			skip: 0,
-			take: total
+			take: total,
+			include: {
+				streams: {
+					include: {
+						user: true,
+						category: true
+					}
+				}
+			}
 		})
 
 		return Array.from(randomIndexes).map(idx => categories[idx])
